6e69a4ed9fb4f2091d38f393e94efc3503503b7d
Denys Chavez <dvchf26@gmail.com>
2024-07-11 17:39:57 -0400

    Part4 a.Structure of backend application, introduction to testing: Exercise 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
   return 1
 };
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((total, currentObject) => {
+    return total + (currentObject.likes || 0)
+  }, 0)
+}
+
+
+
 module.exports = {
-  dummy,
+  dummy, totalLikes
 };
